06b59ad26eae2570d2df534a8451b92370a0cc39
adam-street <adam.street.dev@gmail.com>
2022-09-20 17:14:50 -0500

    added counter component

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.css']
 })
-export class CounterComponent implements OnInit {
+export class CounterComponent {
 
-  constructor() { }
+  count: number = 0;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  onClick(): void {
+    this.count += 1
   }
+
 
 }
